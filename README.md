00370f2d77454ec11fa68825d28f0b0ecf7acbe9
Evan Maddock <maddock.evan@vivaldi.net>
2021-05-11 13:34:46 -0400

    Initial commit with copy subcommand
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

---
+++ README.md
@@ -0,0 +1,19 @@
+A small tool to make packaging for Solus even easier.
+
+## Features
+
+- Copy eopkg files to the default local repo and re-index the repo.
+
+## Usage
+
+`./soltools subcommand`
+
+Available subcommands:
+
+- `copy`
+
+## License
+
+Copyright &copy; 2021 Evan Maddock maddock.evan@vivaldi.net
+
+`soltools` is available under the terms of the Apache 2.0 license.
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use fs_extra::copy_items;
+use fs_extra::dir::CopyOptions;
+use glob::glob;
+use std::{error::Error, path::PathBuf};
+use std::env;
+use std::process;
+use std::process::Command;
+
+const LOCAL_REPO_PATH: &str = "/var/lib/solbuild/local";
+
+fn copy_packages(current_dir: PathBuf) -> Result<(), Box<dyn Error>> {
+    let mut packages = Vec::new();
+
+    let search = format!("{}/*.eopkg", current_dir.to_str().unwrap());
+    for entry in glob(&search).unwrap() {
+        match entry {
+            Ok(path) => {
+                packages.push(path);
+            },
+            Err(e) => return Err(Box::new(e)),
+        }
+    }
+
+    let mut options = CopyOptions::new();
+    options.overwrite = true;
+
+    match copy_items(&packages, LOCAL_REPO_PATH, &options) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+fn index_repo() -> Result<(), Box<dyn Error>> {
+    let status = Command::new("eopkg")
+        .current_dir(LOCAL_REPO_PATH)
+        .arg("index")
+        .arg("--skip-signing")
+        .arg(LOCAL_REPO_PATH)
+        .status();
+
+    match status {
+        Ok(_) => Ok(()),
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Invalid arguments");
+        process::exit(1);
+    }
+
+    let current_dir = match env::current_dir() {
+        Ok(dir) => dir,
+        Err(e) => {
+            eprintln!("Error getting current working directory: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let command = args[1].as_ref();
+    match command {
+        "copy" => {
+            if let Err(e) = copy_packages(current_dir) {
+                eprintln!("Error copying packages to local repo: {}", e);
+                process::exit(1);
+            }
+
+            if let Err(e) = index_repo() {
+                eprintln!("Error indexing local repo: {}", e);
+                process::exit(1);
+            }
+        },
+        _ => {
+            eprintln!("Unknown subcommand: {}", command);
+            process::exit(1);
+        }
+    }
+}
